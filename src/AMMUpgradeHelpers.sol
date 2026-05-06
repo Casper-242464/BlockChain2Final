b71448f77bd7a604d8424c2b7acc88ecd46654df
Casper-242464 <242464@astanait.edu.kz>
2026-05-06 20:17:15 +0500

    feat(AMMUpgradeHelpers): abstract contract suite for OZ compatibility implemented

--- src/AMMUpgradeHelpers.sol
+++ src/AMMUpgradeHelpers.sol
@@ -71,3 +71,217 @@ library AddressUpgradeable {
         }
     }
 }
+
+library StorageSlotUpgradeable {
+    struct AddressSlot {
+        address value;
+    }
+
+    struct BooleanSlot {
+        bool value;
+    }
+
+    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+}
+
+abstract contract ERC1967UpgradeUpgradeable is Initializable {
+    bytes32 internal constant _ROLLBACK_SLOT = bytes32(uint256(keccak256("eip1967.proxy.rollback")) - 1);
+    bytes32 internal constant _IMPLEMENTATION_SLOT = bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1);
+
+    event Upgraded(address indexed implementation);
+
+    function __ERC1967Upgrade_init() internal onlyInitializing {}
+
+    function _getImplementation() internal view returns (address) {
+        return StorageSlotUpgradeable.getAddressSlot(_IMPLEMENTATION_SLOT).value;
+    }
+
+    function _setImplementation(address newImplementation) private {
+        require(AddressUpgradeable.isContract(newImplementation), "ERC1967: new implementation is not a contract");
+        StorageSlotUpgradeable.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
+    }
+
+    function _upgradeTo(address newImplementation) internal {
+        _setImplementation(newImplementation);
+        emit Upgraded(newImplementation);
+    }
+
+    function _upgradeToAndCallSecure(
+        address newImplementation,
+        bytes memory data,
+        bool forceCall
+    ) internal {
+        address oldImplementation = _getImplementation();
+        _setImplementation(newImplementation);
+        if (forceCall && data.length > 0) {
+            _functionDelegateCall(newImplementation, data);
+        }
+
+        if (!StorageSlotUpgradeable.getBooleanSlot(_ROLLBACK_SLOT).value) {
+            StorageSlotUpgradeable.getBooleanSlot(_ROLLBACK_SLOT).value = true;
+            _functionDelegateCall(newImplementation, abi.encodeWithSignature("upgradeTo(address)", oldImplementation));
+            StorageSlotUpgradeable.getBooleanSlot(_ROLLBACK_SLOT).value = false;
+            require(_getImplementation() == oldImplementation, "ERC1967: upgrade breaks further upgrades");
+            _upgradeTo(newImplementation);
+        }
+    }
+
+    function _functionDelegateCall(address target, bytes memory data) private returns (bytes memory) {
+        return AddressUpgradeable.functionDelegateCall(target, data, "ERC1967: delegate call failed");
+    }
+}
+
+interface IERC1822ProxiableUpgradeable {
+    function proxiableUUID() external view returns (bytes32);
+}
+
+abstract contract UUPSUpgradeable is Initializable, ERC1967UpgradeUpgradeable {
+    address private immutable __self = address(this);
+
+    modifier onlyProxy() {
+        require(address(this) != __self, "UUPSUpgradeable: must be called through delegatecall");
+        require(_getImplementation() == __self, "UUPSUpgradeable: must be called through active proxy");
+        _;
+    }
+
+    modifier notDelegated() {
+        require(address(this) == __self, "UUPSUpgradeable: must not be called through delegatecall");
+        _;
+    }
+
+    function __UUPSUpgradeable_init() internal onlyInitializing {
+        __ERC1967Upgrade_init();
+    }
+
+    function upgradeTo(address newImplementation) external virtual onlyProxy {
+        _authorizeUpgrade(newImplementation);
+        _upgradeToAndCallSecure(newImplementation, bytes(""), false);
+    }
+
+    function upgradeToAndCall(address newImplementation, bytes memory data) external payable virtual onlyProxy {
+        _authorizeUpgrade(newImplementation);
+        _upgradeToAndCallSecure(newImplementation, data, true);
+    }
+
+    function proxiableUUID() external view virtual notDelegated returns (bytes32) {
+        return _IMPLEMENTATION_SLOT;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal virtual;
+}
+
+abstract contract OwnableUpgradeable is Initializable, ContextUpgradeable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function __Ownable_init() internal onlyInitializing {
+        __Context_init();
+        __Ownable_init_unchained();
+    }
+
+    function __Ownable_init_unchained() internal onlyInitializing {
+        _transferOwnership(_msgSender());
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+
+    uint256[49] private __gap;
+}
+
+abstract contract Ownable2StepUpgradeable is Initializable, OwnableUpgradeable {
+    address private _pendingOwner;
+
+    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
+
+    function __Ownable2Step_init() internal onlyInitializing {
+        __Ownable_init_unchained();
+    }
+
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
+    }
+
+    function transferOwnership(address newOwner) public virtual override onlyOwner {
+        require(newOwner != address(0), "Ownable2Step: new owner is the zero address");
+        _pendingOwner = newOwner;
+        emit OwnershipTransferStarted(owner(), newOwner);
+    }
+
+    function acceptOwnership() public virtual {
+        address sender = _msgSender();
+        require(sender == _pendingOwner, "Ownable2Step: caller is not the pending owner");
+        _pendingOwner = address(0);
+        _transferOwnership(sender);
+    }
+
+    uint256[49] private __gap;
+}
+
+abstract contract ReentrancyGuardUpgradeable is Initializable {
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+    uint256 private _status;
+
+    function __ReentrancyGuard_init() internal onlyInitializing {
+        _status = _NOT_ENTERED;
+    }
+
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+        _;
+        _status = _NOT_ENTERED;
+    }
+
+    uint256[49] private __gap;
+}
+
+abstract contract ReentrancyGuard {
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+        _;
+        _status = _NOT_ENTERED;
+    }
+}
