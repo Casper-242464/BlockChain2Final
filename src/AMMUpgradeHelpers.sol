f94758866495b2ec96b24e45d660101ae046784f
Casper-242464 <242464@astanait.edu.kz>
2026-05-06 20:15:49 +0500

    feat(AMMUpgradeHelpers): abstract Upgradeable contract implemented

--- src/AMMUpgradeHelpers.sol
+++ src/AMMUpgradeHelpers.sol
@@ -23,3 +23,16 @@ abstract contract Initializable {
         _;
     }
 }
+
+abstract contract ContextUpgradeable is Initializable {
+    function __Context_init() internal onlyInitializing {}
+    function __Context_init_unchained() internal onlyInitializing {}
+
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
