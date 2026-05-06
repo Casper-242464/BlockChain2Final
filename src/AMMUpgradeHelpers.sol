f2bfec888b7f3666b58c525485cfcbbdf76d4607
Casper-242464 <242464@astanait.edu.kz>
2026-05-06 20:14:51 +0500

    feat: contract upgrade helper implemented for OpenZeppelin compatible foundation

---
+++ src/AMMUpgradeHelpers.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+abstract contract Initializable {
+    bool private _initialized;
+    bool private _initializing;
+
+    modifier initializer() {
+        require(_initializing || !_initialized, "Initializable: already initialized");
+        bool isTopLevelCall = !_initializing;
+        if (isTopLevelCall) {
+            _initializing = true;
+            _initialized = true;
+        }
+        _;
+        if (isTopLevelCall) {
+            _initializing = false;
+        }
+    }
+
+    modifier onlyInitializing() {
+        require(_initializing, "Initializable: not initializing");
+        _;
+    }
+}
