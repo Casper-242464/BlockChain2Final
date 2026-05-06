82e75c4440ab8500beb3a6e9eefc194de78dae51
Casper-242464 <242464@astanait.edu.kz>
2026-05-06 20:16:15 +0500

    feat(AMMUpgradeHelpers): abstract AddressUpgradeable contract implemented

--- src/AMMUpgradeHelpers.sol
+++ src/AMMUpgradeHelpers.sol
@@ -36,3 +36,38 @@ abstract contract ContextUpgradeable is Initializable {
         return msg.data;
     }
 }
+
+library AddressUpgradeable {
+    function isContract(address account) internal view returns (bool) {
+        return account.code.length > 0;
+    }
+
+    function functionDelegateCall(address target, bytes memory data) internal returns (bytes memory) {
+        return functionDelegateCall(target, data, "Address: low-level delegate call failed");
+    }
+
+    function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        require(isContract(target), "Address: delegate call to non-contract");
+
+        (bool success, bytes memory returndata) = target.delegatecall(data);
+        return verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function verifyCallResult(
+        bool success,
+        bytes memory returndata,
+        string memory errorMessage
+    ) internal pure returns (bytes memory) {
+        if (success) {
+            return returndata;
+        }
+        if (returndata.length > 0) {
+            assembly {
+                let returndata_size := mload(returndata)
+                revert(add(32, returndata), returndata_size)
+            }
+        } else {
+            revert(errorMessage);
+        }
+    }
+}
