fb69ae8d0af9542790f91ac2594fa7ed81133fa9
Baknur1 <baknuraitimbet@gmail.com>
2026-05-16 16:04:09 +0500

    implement DSATimelock contract and constructor

---
+++ src/governance/DSATimelock.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {
+    TimelockController
+} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract DSATimelock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    ) TimelockController(minDelay, proposers, executors, admin) {}
+}
